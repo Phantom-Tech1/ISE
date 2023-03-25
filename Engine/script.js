0a7bd1679bb8437f96701dbb1d76e731ed164627
Jack Geraghty <97810268+ViolentCovid@users.noreply.github.com>
2023-03-25 10:28:56 -0400

    update directions script

--- Engine/script.js
+++ Engine/script.js
@@ -9,6 +9,7 @@ let url = location;
 1. Inside the quotes when assigning the value of the searchBar, input what you want to be searched in the searchBar to be brought to the url.
 2. Then in the other quotes right below the first value, input the url you want the user to be brought to when the value that you just put in a second ago is searched.
 3. Don't forget to update the key in the index.html file. More instructions will be put there.
+4. The site will not work with extra else if statements, so when you need more copy paste more, when you finish editing delete all of the if else statements but leave the else statement at the end.
 */
 function searchable() {
     if (searchBar.value == 'lioxryt') {
