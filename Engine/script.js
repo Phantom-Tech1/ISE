57dd375d42c873fa6d485b8a0ff346a17ef0019c
Jack Geraghty <97810268+ViolentCovid@users.noreply.github.com>
2023-03-25 10:27:18 -0400

    Directions

--- Engine/script.js
+++ Engine/script.js
@@ -3,12 +3,43 @@ const searchBtn = document.querySelector('.searchBtn');
 let url = location;
 
 //Supa Function\\
+
+//Directions\\
+/*
+1. Inside the quotes when assigning the value of the searchBar, input what you want to be searched in the searchBar to be brought to the url.
+2. Then in the other quotes right below the first value, input the url you want the user to be brought to when the value that you just put in a second ago is searched.
+3. Don't forget to update the key in the index.html file. More instructions will be put there.
+*/
 function searchable() {
     if (searchBar.value == 'lioxryt') {
         location.href = "https://violent-covids-lioxryt-48576.netlify.app"
     } else if (searchBar.value == "3kh0") {
         location.href = "https://violent-covids-3kh0-59687.netlify.app"
-    } else {
+    } else if (searchBar.value == "") {
+        location.href = ""
+    } else if (searchBar.value == "") {
+        location.href = ""
+    } else if (searchBar.value == "") {
+        location.href = ""
+    } else if (searchBar.value == "") {
+        location.href = ""
+    } else if (searchBar.value == "") {
+        location.href = ""
+    } else if (searchBar.value == "") {
+        location.href = ""
+    } else if (searchBar.value == "") {
+        location.href = ""
+    } else if (searchBar.value == "") {
+        location.href = ""
+    } else if (searchBar.value == "") {
+        location.href = ""
+    } else if (searchBar.value == "") {
+        location.href = ""
+    } else if (searchBar.value == "") {
+        location.href = ""
+    } else if (searchBar.value == "") {
+        location.href = ""
+    }else {
         searchBar.placeholder = "word invalid, vist KEY";
     }
 };
