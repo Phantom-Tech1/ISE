423de6640653bc7681ecb7033bc4d17436a693ab
Jack Geraghty <97810268+Corrupt-Hacker@users.noreply.github.com>
2023-04-09 18:52:24 -0400

    READY FOR DEPLOYMENT

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ let url = location;
 */
 function searchable() {
     if (searchBar.value == 'lioxryt') {
-        location.href = "https://violent-covids-lioxryt-48576.netlify.app"
+        location.href = "https://corrupted-hackers-lioxryt.netlify.app"
     } else if (searchBar.value == "3kh0") {
-        location.href = "https://violent-covids-3kh0-59687.netlify.app"
+        location.href = "https://corrupted-hacker-3kh0.netlify.app"
     } else if (searchBar.value == "bigfoot") {
-        location.href = "https://violent-covids-bigfoot-15243.netlify.app/"
+        location.href = "https://corrupted-hackers-bigfoot.netlify.app/"
     } else if (searchBar.value == "cookie") {
         location.href = "https://www.google.com/search?q=cookie&rlz=1CASICM_enUS1045&oq=cookie&aqs=chrome..69i57j46i395i409i424i433i512j46i67i395i415i424i433i650j0i67i650j0i433i512j46i67i433i650j46i433i512j0i131i433i512j0i433i512j0i271.1461j1j7&sourceid=chrome&ie=UTF-8"
     } else if (searchBar.value == "school server") {
@@ -27,19 +27,53 @@ function searchable() {
     } else if (searchBar.value == "hammerhead2") {
         location.href = "https://udemy.gq/"
     } else if (searchBar.value == "bloxors") {
-        location.href = "https://violent-covids-lioxryt-48576.netlify.app/flash/?swf=bloxorz.swf"
+        location.href = "https://corrupted-hackers-lioxryt/flash/?swf=bloxorz.swf"
     } else if (searchBar.value == "duck hunt") {
-        location.href = "https://violent-covids-lioxryt-48576.netlify.app/flash/?swf=duckhunt.swf"
+        location.href = "https://corrupted-hackers-lioxryt/flash/?swf=duckhunt.swf"
     } else if (searchBar.value == "hobo 7 heaven") {
-        location.href = "https://violent-covids-lioxryt-48576.netlify.app/flash/?swf=hobo7.swf"
+        location.href = "https://corrupted-hackers-lioxryt/flash/?swf=hobo7.swf"
     } else if (searchBar.value == "pac man") {
-        location.href = "https://violent-covids-lioxryt-48576.netlify.app/flash/?swf=pacman.swf"
+        location.href = "https://corrupted-hackers-lioxryt/flash/?swf=pacman.swf"
     } else if (searchBar.value == "pong") {
-        location.href = "https://violent-covids-lioxryt-48576.netlify.app/flash/?swf=pong.swf"
+        location.href = "https://corrupted-hackers-lioxryt/flash/?swf=pong.swf"
     } else if (searchBar.value == "tetris") {
-        location.href = "https://violent-covids-lioxryt-48576.netlify.app/flash/?swf=flash-tetris.swf"
+        location.href = "https://corrupted-hackers-lioxryt/flash/?swf=flash-tetris.swf"
     } else if (searchBar.value == "worlds hardest game 2") {
-        location.href = "https://violent-covids-lioxryt-48576.netlify.app/flash/?swf=theworldshardestgame2.swf"
+        location.href = "https://corrupted-hackers-lioxryt/flash/?swf=theworldshardestgame2.swf"
+    } else if (searchBar.value = "8 ball pool") {
+        location.href = "https://corrupted-hackers-bigfoot.netlify.app/g/loading/index.html?url=gfiles/gfiles/flash/?swf=8BallPool.swf"
+    } else if (searchBar.value = "bow master") {
+        location.href = "https://corrupted-hackers-bigfoot.netlify.app/g/loading/index.html?url=gfiles/gfiles/flash/?swf=bowmaster.swf"
+    } else if (searchBar.value = "axis football league") {
+        location.href = "https://corrupted-hackers-bigfoot.netlify.app/g/gfiles/gfiles/flash/?swf=axisfootballleague.swf"
+    } else if (searchBar.value = "2048") {
+        location.href = "https://corrupted-hackers-bigfoot.netlify.app/g/gfiles/gfiles/html5/2048/index.html"
+    } else if (searchBar.value = "1 on 1 soccer") {
+        location.href = "https://corrupted-hackers-bigfoot.netlify.app/g/loading/index.html?url=gfiles/gfiles/flash/?swf=1on1soccer.swf"
+    } else if (searchBar.value = "50 states") {
+        location.href = "https://corrupted-hackers-bigfoot.netlify.app/g/loading/index.html?url=gfiles/gfiles/flash/?swf=50states.swf"
+    } else if (searchBar.value = "3 on 3 hockey") {
+        location.href = "https://corrupted-hackers-bigfoot.netlify.app/g/loading/index.html?url=gfiles/gfiles/flash/?swf=3_on_3_hockey.swf"
+    } else if (searchBar.value = "apple shooter") {
+        location.href = "https://corrupted-hackers-bigfoot.netlify.app/g/loading/index.html?url=gfiles/gfiles/flash/?swf=apple-shooter-2021.swf"
+    } else if (searchBar.value = "bullet bill 3") {
+        location.href = "https://corrupted-hackers-bigfoot.netlify.app/g/loading/index.html?url=gfiles/gfiles/flash/?swf=Bullet%20Bill%203.swf"
+    } else if (searchBar.value = "chess") {
+        location.href = "https://corrupted-hackers-bigfoot.netlify.app/g/loading/index.html?url=gfiles/gfiles/html5/chess/index.html"
+    } else if (searchBar.value = "fat ninja") {
+        location.href = "https://corrupted-hackers-bigfoot.netlify.app/g/gfiles/gfiles/flash/?swf=fat-ninja.swf"
+    } else if (searchBar.value = "gun blood") {
+        location.href = "https://corrupted-hackers-lioxryt.netlify.app/flash/?swf=1gunblood.swf"
+    } else if (searchBar.value = "1v1 lol") {
+        location.href = "https://corrupted-hacker-3kh0.netlify.app/files/1v1lol/index.html"
+    } else if (searchBar.value = "basketball random") {
+        location.href = "https://corrupted-hacker-3kh0.netlify.app/files/basket-random/index.html"
+    } else if (searchBar.value = "core ball") {
+        location.href = "https://corrupted-hacker-3kh0.netlify.app/files/core-ball/index.html"
+    } else if (searchBar.value = "cluster rush") {
+        location.href = "https://corrupted-hacker-3kh0.netlify.app/files/cluster-rush/index.html"
+    } else if (searchBar.value = "crossy road") {
+        location.href = "https://corrupted-hacker-3kh0.netlify.app/files/crossyroad/index.html"
     } else {
         searchBar.placeholder = "word invalid, vist KEY";
     }
